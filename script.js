5162c20fe658c32a217f976f2d7624d0a775e67a
davidstroh <dstroh10@gmail.com>
2017-09-05 08:33:44 -0500

    working out window screen algorithm to work w/ multiple gates stored within the same array

--- script.js
+++ script.js
@@ -96,6 +96,111 @@ availability.forEach(function(d, i) {
 })
 
 
+/**
+ * Returns a sliding window over data
+ * with lenth len, starting from cur
+ * repeating data infinitely to fit the window
+ */
+function infiniteSlidingWindow(data, cur, len) {
+    var win = [],
+        num = data.length,
+        numVisible = len;
+    // A negative cur is the same as num - abs(cur)
+    if (cur < 0) {
+        cur = num + cur;
+    }
+    // Now keep adding items until we have enough
+    var index = 0;
+
+    //console.log(0?"zero":win.length)
+    while (win.length < numVisible) {
+        var first = (win.length?0:Math.abs(cur)%num),
+            missing = numVisible - win.length,
+            last = Math.min(first + missing, num);
+        win = win.concat(data.slice(first, last));
+        //console.log(++index);
+        //console.log(win);
+    }
+    return win;
+}
+
+/* Tests */
+var data = [0,1,2,3,4,5,6,7,8,9];
+var result;
+
+result = infiniteSlidingWindow(data, 6, 17);
+//console.log(result);
+
+
+var incrementNum = 48 * 7;
+function test(data, cur, numVisible) {
+    var win=[];
+
+    if(cur < 0) {
+      cur = incrementNum + cur;
+    }
+
+    while (win.length < numVisible) {
+        var first = (win.length ? 0 : Math.abs(cur)%incrementNum),
+            missing = numVisible - win.length,
+            dataColNum = data.length / incrementNum,
+            //winColNum = Math.floor(win.length / dataColNum),
+            last = Math.min(first + missing, incrementNum)
+            i = first;
+        while (i < last) {
+          var j = 0;
+              //winColNum = Math.floor(win.length / dataColNum);
+          //console.log(i +' : '+ winColNum +' : '+ j)
+          while (j < dataColNum) {
+            winColNum = Math.floor(win.length / dataColNum);
+            console.log(i +' : '+ winColNum +' : '+ j)
+            var ind = (win.length > dataColNum ? winColNum * (j+1) : j);
+            win.splice(ind, 0, data[i+ (48*j)]);
+            j++;
+            //winColNum = Math.floor(win.length / dataColNum);
+          }
+          i++;
+        }//console.log(win.length +" : "+ numVisible);
+        //break;
+    }
+
+    return win;
+}
+
+console.log(test(availability, 0, 24));
+
+
+
+
+
+/*function test(data, first, last) {
+  var arr = [],
+  numVisible = last - first,
+  i = first,
+  rowInd = 1;
+
+  while( i < 7*8*last ) {
+
+    while ( i < last + (48 * rowInd) ) {
+      console.log(numVisible*rowInd);
+      arr.splice( numVisible*rowInd - numVisible%, 0, data[i]);
+      i++;
+    }
+
+    i = 10000000000000;//first + (48 * rowInd);
+  }
+
+  return arr;
+}*/
+
+//var data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90];
+//console.log(test(data, 3, 6));
+
+
+
+
+
+
 /*while(i < availability.length) {
     if(i < 36) {
         arr.push(availability[i]);
@@ -106,7 +211,7 @@ availability.forEach(function(d, i) {
     i++;
 }*/
 
-console.log(arr);
+//console.log(arr);
 
 
 
@@ -214,20 +319,3 @@ newData = data.filter(function(d) {
 
 
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
