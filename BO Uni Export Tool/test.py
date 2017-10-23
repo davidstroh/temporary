37c035824ea9690cc80c51c09afe72adfa0cb919
davidstroh <dstroh10@gmail.com>
2017-10-22 21:08:01 -0500

    restructuring dimension object

--- BO Uni Export Tool/test.py
+++ BO Uni Export Tool/test.py
@@ -63,38 +63,135 @@ class boItem:
 		self.coreItemIdentifier = None
 		self.overridableProperties = None
 		self.businessName = None
-		self.description = 'Empty'
+		self.description = None
 		self.state = None
 		self.dataType = None
 		self.access = None
 		self.forResult = None
 		self.sqlDefinition = None
+		self.listOfValues = None
 		self.canBeUsedIn = None
 
+		self.previousAttr = None
+
+
+	def setPrevAttr(self, prev):
+		self.previousAttr = prev
+	def getPrevAttr(self):
+		return self.previousAttr
+
+	def setAttr(self, type, value):
+		value = value.replace(type + ' :','').lstrip()
+
+		if('coreItemIdentifier' in type):
+			self.coreItemIdentifier = value if value else 'Empty'
+			self.previousAttr = 'coreItemIdentifier'
+		elif('overridableProperties' in type):
+			self.overridableProperties = value if value else self.overridableProperties
+			self.previousAttr = 'overridableProperties'
+		elif('businessName' in type):
+			self.businessName = value if value else self.businessName
+			self.previousAttr = 'businessName'
+		elif('description' in type):
+			self.description = value if value else ''
+			self.previousAttr = 'description'
+		elif('state' in type):
+			self.state = value if value else self.state
+			self.previousAttr = 'state'
+		elif('dataType' in type):
+			self.dataType = value if value else self.dataType
+			self.previousAttr = 'dataType'
+		elif('access' in type):
+			self.access = value if value else self.access
+			self.previousAttr = 'access'
+		elif('forResult' in type):
+			self.forResult = value if value else self.forResult
+			self.previousAttr = 'forResult'
+		elif('SQL Definition' in type):
+			self.sqlDefinition = value if value else self.sqlDefinition
+			self.previousAttr = 'SQL Definition'
+		elif('Can be used in' in type):
+			self.canBeUsedIn = value if value else self.canBeUsedIn
+			self.previousAttr = 'Can be used in'
+
+		self.previousAttr = type
+
+
 	def setCoreItemIdentifier(self, value):
+		value = value.replace('coreItemIdentifier :','').lstrip()
 		self.coreItemIdentifier = value if value else 'Empty'
+		self.previousAttr = 'coreItemIdentifier'
 	def getCoreItemIdentifier(self):
 		return self.coreItemIdentifier
 	def setOverridableProperties(self, value):
+		value = value.replace('overridableProperties :','').lstrip()
 		self.overridableProperties = value if value else self.overridableProperties
+		self.previousAttr = 'overridableProperties'
+	def getOverridableProperties(self):
+		return self.overridableProperties
 	def setBusinessName(self, value):
+		value = value.replace('businessName :','').lstrip()
 		self.businessName = value if value else self.businessName
+		self.previousAttr = 'businessName'
 	def setDescription(self, value):
-		self.description = value if value else self.description
+		value = value.replace('description :','').lstrip()
+		self.description = value if value else ''
+		self.previousAttr = 'description'
 	def setState(self, value):
+		value = value.replace('state :','').lstrip()
 		self.state = value if value else self.state
+		self.previousAttr = 'state'
 	def getState(self):
 		return self.state
 	def setDataType(self, value):
+		value = value.replace('dataType :','').lstrip()
 		self.dataType = value if value else self.dataType
+		self.previousAttr = 'dataType'
 	def setAccess(self, value):
+		value = value.replace('access :','').lstrip()
 		self.access = value if value else self.access
+		self.previousAttr = 'access'
 	def setForResult(self, value):
+		value = value.replace('forResult :','').lstrip()
 		self.forResult = value if value else self.forResult
+		self.previousAttr = 'forResult'
 	def setSqlDefinition(self, value):
+		value = value.replace('SQL Definition :','').lstrip()
 		self.sqlDefinition = value if value else self.sqlDefinition
+		self.previousAttr = 'SQL Definition'
+	def setListOfValues(self, value):
+		value = value.replace('List of Values :','').lstrip()
+		self.listOfValues = value if value else self.listOfValues
+		self.previousAttr = 'List of Values'
 	def setCanBeUsedIn(self, value):
+		value = value.replace('Can be used in :','').lstrip()
 		self.canBeUsedIn = value if value else self.canBeUsedIn
+		self.previousAttr = 'Can be used in'
+
+	def appendAttribute(self, type, value):
+		if('coreItemIdentifier' in type):
+			self.coreItemIdentifier += value
+		elif('overridableProperties' in type):
+			self.overridableProperties += value
+		elif('businessName' in type):
+			self.businessName += value
+		elif('description' in type):
+			self.description += value
+		elif('state' in type):
+			self.state += value
+		elif('dataType' in type):
+			self.dataType += value
+		elif('access' in type):
+			self.access += value
+		elif('forResult' in type):
+			self.forResult += value
+		elif('SQL Definition' in type):
+			self.sqlDefinition  += value
+		elif('Can be used in' in type):
+			self.canBeUsedIn += value
+
+
+
 
 	def printAll(self):
 		toPrint = [str(self.coreItemIdentifier),str(self.overridableProperties),str(self.businessName),str(self.description),str(self.state),str(self.dataType),str(self.access),str(self.forResult),str(self.sqlDefinition),str(self.canBeUsedIn)]
@@ -136,7 +233,7 @@ with open('HRInfoUni.txt') as file:
 	inDimAttrGroup = 0 # indicates whether current parsed line is within the Dimensions and Attributes Section
 	inIndiDimGroup = 0 # indicates whether current parsed line is within an individual dimension grouping
 	isBlankLine = 0 # indicates whether the parsed line is blank to be used in the next iteration
-	prevLine = blank
+	prevLine = None
 	skipNextLine = 0
 	dimensionList = []
 
@@ -183,27 +280,49 @@ with open('HRInfoUni.txt') as file:
 				line = line.lstrip().rstrip('\n')
 
 				count=count+1
-				if(inIndiDimGroup = 0 and 'Dimension' in line and '-------------------------------------------------' in next(file)):
+				if(inIndiDimGroup == 0 and 'Dimension' in line and '-------------------------------------------------' in next(file)):
 					inIndiDimGroup = 1
 					line = line.replace('Dimension: ', '')
 					newDim = boItem(line)
 					prevLine = next(file).lstrip().rstrip('\n')
 					count = count+1
 
-				elif(inIndiDimGroup = 1):
+				elif(inIndiDimGroup == 1):
 
-					if('coreItemIdentifier' in list):
-						newDim.setCoreItemIdentifier(list.replace('coreItemIdentifier :','').lstrip())
-					elif('overridableProperties' in list):
-						newDim.setOverridableProperties(list.replace('overridableProperties :','').lstrip())
-					elif('businessName' in list):
-						newDim.setBusinessName(list.replace('businessName :','').lstrip())
-					elif('description' in list):
-						newDim.setDescription(list.replace('description :','').lstrip())
-					elif(not list):
+					if('coreItemIdentifier' in line):
+						newDim.setAttr('coreItemIdentifier', line)
+					elif('overridableProperties' in line):
+						newDim.setAttr('overridableProperties', line)
+					elif('businessName' in line):
+						newDim.setAttr('businessName', line)
+					elif('description' in line):
+						newDim.setAttr('description', line)
+					elif('state' in line):
+						newDim.setAttr('state', line)
+					elif('dataType' in line):
+						newDim.setAttr('dataType', line)
+					elif('access' in line):
+						newDim.setAttr('access', line)
+					elif('forResult' in line):
+						newDim.setAttr('forResult', line)
+					elif('SQL Definition' in line):
+						newDim.setAttr('SQL Definition', line)
+					elif('List of Values' in line):
+						newDim.setAttr('List of Values', line)
+					elif('Can be used in' in line):
+						newDim.setAttr('Can be used in', line)
+
+					elif not line:
+						print('HERE: '+ line)
+						inIndiDimGroup = 0
+						dimensionList.append(newDim)
+					else:
+						print('')
 
 
-'''					li=next(file).rstrip('\n')
+
+					'''
+					li=next(file).rstrip('\n')
 					count=count+1
 					if('coreItemIdentifier' in li):
 						newDim.setCoreItemIdentifier(li.replace('coreItemIdentifier :','').lstrip())
@@ -261,11 +380,11 @@ with open('HRInfoUni.txt') as file:
 					dimensionList.append(newDim)
 					if newDim.getMissingData():
 						print(str(count) + ': '+ str(newDim.getMissingData()))
-'''
+					'''
 
 
 #busObj.printAll()
-print(errorCount)
+print(len(dimensionList))
 
 
 
